b521668a7084171efab8ac7f096f5e3ba35fbb81
Marifah <marifahalifah@gmail.com>
2024-11-21 12:39:52 +0800

    ml web server

--- src/inference.js
+++ src/inference.js
@@ -1,20 +1,18 @@
 const tfjs = require('@tensorflow/tfjs-node');
-
-// load model tensorflow.js
+ 
 function loadModel() {
-    const modelUrl = "file://models/model.json";
-    return tfjs.loadLayersModel(modelUrl);
-  }
-
-// predict() untuk memprediksi data (berupa imageBuffer) dengan model yang telah di-load
+  const modelUrl = "file://models/model.json";
+  return tfjs.loadLayersModel(modelUrl);
+}
+ 
 function predict(model, imageBuffer) {
-    const tensor = tfjs.node
-      .decodeJpeg(imageBuffer)
-      .resizeNearestNeighbor([150, 150])
-      .expandDims()
-      .toFloat();
-   
-    return model.predict(tensor).data();
-  }
-
-  module.exports = { loadModel, predict };
+  const tensor = tfjs.node
+    .decodeJpeg(imageBuffer)
+    .resizeNearestNeighbor([150, 150])
+    .expandDims()
+    .toFloat();
+ 
+  return model.predict(tensor).data();
+}
+ 
+module.exports = { loadModel, predict };
